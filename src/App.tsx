4fbcd965a6f7c8edbf64514343b6ebc2a0f7fe7d
radari <reda.adari3@docaposte.fr>
2025-02-24 03:20:08 +0100

    rename pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "@/pages/home";
-import { Contact } from "@/pages/contact";
+import { Home } from "@/pages/Home";
+import { Contact } from "@/pages/Contact";
 import { Suspense } from "react";
 import { Spinner } from "@chakra-ui/react";
 import { Layout } from "@/components/Layout";
